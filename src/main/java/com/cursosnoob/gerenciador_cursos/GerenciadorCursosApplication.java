0f7df7399be29b2ba507c726946e72ec19d1bdb8
Samuel Monteiro <ss-monteiro@outlook.com>
2025-07-06 19:12:14 -0400

    Commit inicial do projeto Gerenciador de Cursos NOOB

---
+++ src/main/java/com/cursosnoob/gerenciador_cursos/GerenciadorCursosApplication.java
@@ -0,0 +1,13 @@
+package com.cursosnoob.gerenciador_cursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorCursosApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/cursosnoob/model/Curso.java
@@ -0,0 +1,44 @@
+package com.cursosnoob.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity // Anotação que informa ao JPA que esta classe é uma entidade.
+public class Curso {
+
+    @Id // Define o campo como a chave primária da tabela.
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Gera o valor do ID automaticamente.
+    private Long id;
+
+    private String nome;
+    private int cargaHoraria;
+
+    // Construtores, Getters e Setters (essenciais para o framework)
+    public Curso() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+}
